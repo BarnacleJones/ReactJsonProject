539c65c3db6c959d7486c01f5be0803888bb7db0
BarnacleJones <benmjones@protonmail.com>
2022-08-03 10:56:54 +1200

    further styling

--- src/Components/HomeCards.js
+++ src/Components/HomeCards.js
@@ -2,7 +2,7 @@ import Content from "../scp.json";
 
 const SCPCards = Content.map(
   (file) => {return(
-    <div key={file.Item} className="card" id="cardImage">  
+    <div key={file.Item} className="card bg-secondary mb-3" id="cardImage">  
   <img src={file.Image} className="card-img-top" alt="..." />
   <div className="card-body">
     <h5 className="card-title">{file.Item}</h5>   
